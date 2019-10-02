a86937a8e1bd2b1d5ab50fa737d6887c17b28500
Vania <vrolo001@gmail.com>
2019-10-02 13:06:20 +0100

    section 1.4 started, completed halfway

--- Section 1 - Discrete Probability.R
+++ Section 1 - Discrete Probability.R
@@ -21,7 +21,6 @@ balls <- rep(c("cyan", "magenta", "yellow"), times = c(3,5,7))
     3/15 * 12/15 #alternative
   
 #### Assessment: Intro to Discrete Probability (datacamp; same problems as edx only solved differently)
-    
     cyan <- 3
     magenta <- 5
     yellow <- 7
@@ -153,3 +152,111 @@ prob_win <- function(N, p=0.75){
 
 Pr <- sapply(N, prob_win)
 plot(N,Pr)
+
+###### Section 1.4: Assessment: Discrete Probability
+library(gtools)
+library(tidyverse)
+options(digits = 3)
+
+
+### Question 1: Olympic running
+
+#In the 200m dash finals in the Olympics, 8 runners compete for 3 medals (order matters). In the 2012 Olympics, 3 of 8 runners
+#were from Jamaica and the other 5 were from different countries. The three medals were all won by Jamaica (Usain Bolt, Yohan Blake, and Warren Weir).
+
+  #1a:How many different ways can the 3 medals be distributed across 8 runners?
+
+nrow(permutations(8,3))
+
+  #1b: How many different ways can the three medals be distributed among the 3 runners from Jamaica?
+
+nrow(permutations(3,3))
+
+  #1c:What is the probability that all 3 medals are won by Jamaica?
+
+Pr <- nrow(permutations(3,3))/nrow(permutations(8,3))
+
+  #1d: Run a Monte Carlo simulation on the vector below representing the countries of the 8 runners in this race:
+
+runners <- c("Jamaica", "Jamaica", "Jamaica", "USA", "Ecuador", "Netherlands", "France", "South Africa")
+
+    #For each iteration of the Monte Carlo simulation, within a replicate loop, select 3 runners representing the 3 medalists and 
+    #check whether they are all from Jamaica. Repeat this simulation 10,000 times. Set the seed to 1 before running the loop.
+    #Calculate the probability that all the runners are from Jamaica.
+
+set.seed(1)
+B <- 10000
+results <- replicate(B,{
+  runners <- c("Jamaica", "Jamaica", "Jamaica", "USA", "Ecuador", "Netherlands", "France", "South Africa")
+  medalists <- sample(runners, 3)
+  all(medalists == "Jamaica")
+  })
+mean(results)
+
+### Question 2: Restaurant management
+
+#A restaurant manager wants to advertise that his lunch special offers enough choices to eat different meals every day of the year.
+#He wants to change his special if needed to allow at least 365 choices. A meal at the restaurant includes 1 entree, 2 sides, and 1 drink.
+#He currently offers a choice of 1 entree from a list of 6 options, a choice of 2 different sides from a list of 6 options, and a choice of 
+#1 drink from a list of 2 options.
+
+  #2a: How many meal combinations are possible with the current menu?
+
+entree <- 6
+side<- combinations(6,2)
+drink <- 2
+
+entree * nrow(side) * drink
+
+  #2b: The manager has one additional drink he could add.How many combinations are possible if he expands the special to 3 drink options?
+
+entree <- 6
+side<- combinations(6,2)
+drink <- 3
+
+entree * nrow(side) * drink
+
+  #2c: The manager decides to add the third drink but needs to expand the number of options. He would prefer not to change his menu further and 
+  #wants to know if he can meet his goal by letting customers choose more sides. How many meal combinations are there if customers can choose
+  #from 6 entrees, 3 drinks, and select 3 sides from the current 6 options?
+
+entree <- 6
+side<- combinations(6,3)
+drink <- 3
+
+entree * nrow(side) * drink
+
+  #2d: The manager is concerned that customers may not want 3 sides with their meal. He is willing to increase the number of entree choices
+  #instead, but if he adds too many expensive options it could eat into profits. He wants to know how many entree choices he would have to
+  #offer in order to meet his goal.
+  #Write a function that takes a number of entree choices and returns the number of meal combinations possible given that number of entree
+  #options, 3 drink choices, and a selection of 2 sides from 6 options. Use sapply to apply the function to entree option counts ranging
+  #from 1 to 12. What is the minimum number of entree options required in order to generate more than 365 combinations?
+
+n <- 1:12
+meals <- function(n){
+  n * nrow(combinations(6,2)) * 3
+}
+
+total_meals <- sapply(n, meals)
+
+data.frame(entrees = n, meals = total_meals) %>%
+  filter(meals > 365) %>%
+  min(.$entrees)
+
+  #2e: The manager isn't sure he can afford to put that many entree choices on the lunch menu and thinks it would be cheaper for him to 
+  #expand the number of sides. He wants to know how many sides he would have to offer to meet his goal of at least 365 combinations.
+  #Write a function that takes a number of side choices and returns the number of meal combinations possible given 6 entree choices, 
+  #3 drink choices, and a selection of 2 sides from the specified number of side choices. Use sapply to apply the function to side counts
+  #ranging from 2 to 12. What is the minimum number of side options required in order to generate more than 365 combinations?
+
+n <- 2:12
+meals <- function(n){
+  6 * nrow(combinations(n,2)) * 3
+}
+
+total_meals <- sapply(n, meals)
+
+data.frame(sides = n, meals = total_meals) %>%
+  filter(meals > 365) %>%
+  min(.$sides)
